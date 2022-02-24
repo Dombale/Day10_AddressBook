99ef7bb3113bc250140cb6a531cddf1a37bfe812
Priyanka <priyankadombale27@gmail.com>
2022-02-25 05:19:25 +0530

    Added: Edit Existing Contact

--- src/com/blz/AddressBook.java
+++ src/com/blz/AddressBook.java
@@ -28,14 +28,68 @@ public class AddressBook {
 		Contacts personn = new Contacts(fName, lName, address, city, state, zip, phoneNumber, email);
 		// adding personn object into the linked list with Contact class data type
 		person.add(personn);
-		// printing person array list
+		// printing array list
 		System.out.println(person);
+	}
+
+	// editing contact by searching names
+	public void editPerson() {
+		System.out.println("Enter the name to edit");
+		String sc = scanner.next();
+
+		for (int i = 0; i < person.size(); i++) {
+			Contacts personn = person.get(i);
+			if (sc.equals(personn.getFirstName())) {
+				while (true) {
+					System.out.println("Enter choice to edit 1)firstName\n2)lastName\n3)address\n"
+							+ "4)city\n5)state\n6)zip\n7)phoneNumber\n8)email9)exit");
+					int choice = scanner.nextInt();
+					switch (choice) {
+					case 1:
+						personn.setFirstName(scanner.next());
+						break;
+					case 2:
+						personn.setLastName(scanner.next());
+						break;
+					case 3:
+						personn.setAddress(scanner.next());
+						break;
+					case 4:
+						personn.setCity(scanner.next());
+						break;
+					case 5:
+						personn.setState(scanner.next());
+						break;
+					case 6:
+						personn.setZip(scanner.next());
+						break;
+					case 7:
+						personn.setPhoneNumber(scanner.next());
+						break;
+					case 8:
+						personn.setEmail(scanner.next());
+						break;
+					default:
+						System.out.println("select correct choice");
+						break;
+					}
+					if (choice == 9)
+						break;
+				}
+				person.set(i, personn);
+				System.out.println("person after editing");
+				System.out.println(person);
+
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
 		AddressBook addressBook = new AddressBook();
 		System.out.println("Welcome in Addressbook..!!!");
 		addressBook.addPerson();
+		addressBook.editPerson();
 	}
 
 }
